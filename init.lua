70ff7c7d27606ace49c440ae3a6ee3c2b22b0ea4
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-05-26 00:10:40 +0100

    install coq

--- init.lua
+++ init.lua
@@ -568,7 +568,11 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for tsserver)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            --[[ require('lspconfig')[server_name].setup(server) ]]
+            vim.lsp.config(server_name, server)
+            local coq = require 'coq'
+            vim.lsp.config(server_name, coq.lsp_ensure_compatibility(server))
+            vim.lsp.enable(server_name)
           end,
         },
       }

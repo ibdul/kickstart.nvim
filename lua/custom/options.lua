e27f428819a35e91aa9f20ec89fde90eba945e59
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2025-10-04 14:06:53 +0100

    format options file

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -35,31 +35,28 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
-
-
-
 --[[ tab width ]]
 --
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
 --[[ vim.opt.clipboard = "unnamedplus" -- allow nvim to access system clipboard ]]
-vim.opt.cmdheight = 2          -- allow more height in neovim cmd
-vim.opt.fileencoding = "utf-8" -- default encoding
-vim.opt.mouse = "i"            -- allow use of mouse in all modes
+vim.opt.cmdheight = 2 -- allow more height in neovim cmd
+vim.opt.fileencoding = 'utf-8' -- default encoding
+vim.opt.mouse = 'i' -- allow use of mouse in all modes
 vim.opt.termguicolors = true
-vim.opt.pumheight = 10         -- pop up menu height
-vim.opt.showtabline = 2        -- always show tabs
-vim.opt.smartcase = true       --smart casing
-vim.opt.smartindent = true     -- smart indenting
-vim.opt.splitbelow = true      -- force all horizontal spits to go below current window
-vim.opt.splitright = true      -- force all vertical spits to go right of current window
-vim.opt.expandtab = true       -- convert tabs to spaces
-vim.opt.shiftwidth = 2         -- 2 spaces indentations
-vim.opt.tabstop = 2            -- 1 tab == 2 spaces
-vim.opt.cursorline = true      -- underline current line
-vim.opt.number = true          -- add absolute line numbers
-vim.opt.relativenumber = true  -- add relative line numbers
+vim.opt.pumheight = 10 -- pop up menu height
+vim.opt.showtabline = 2 -- always show tabs
+vim.opt.smartcase = true --smart casing
+vim.opt.smartindent = true -- smart indenting
+vim.opt.splitbelow = true -- force all horizontal spits to go below current window
+vim.opt.splitright = true -- force all vertical spits to go right of current window
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 2 -- 2 spaces indentations
+vim.opt.tabstop = 2 -- 1 tab == 2 spaces
+vim.opt.cursorline = true -- underline current line
+vim.opt.number = true -- add absolute line numbers
+vim.opt.relativenumber = true -- add relative line numbers
 vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8      -- scroll offset
-vim.opt.showmode = false       -- hide vim mode display
+vim.opt.sidescrolloff = 8 -- scroll offset
+vim.opt.showmode = false -- hide vim mode display
